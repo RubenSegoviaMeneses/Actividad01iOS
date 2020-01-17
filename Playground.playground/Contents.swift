af088dd54d92b6136ed4d6f5cff29e8e3e0d0fd0
RubenSegoviaMeneses <59990660+RubenSegoviaMeneses@users.noreply.github.com>
2020-01-16 23:24:36 -0600

    Add files via upload

---
+++ Playground.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Foundation
+var v1 = "esto es Swift"
+let c1 = 4 
+var cadena:String = String()
+print("Hola mundo "+v1)
+print("El contenido es: \(cadena)") 
+print("El valor de constante es: \(c1)") 
